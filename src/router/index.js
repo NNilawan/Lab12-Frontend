ec0f1bf5b1b43238bc1aab47b594856483fa2dd0
NNilawan <nilawan_l@cmu.ac.th>
2021-09-30 23:16:43 +0700

    Q3 3.5

--- src/router/index.js
+++ src/router/index.js
@@ -12,111 +12,115 @@ import NProgress from 'nprogress'
 import EventService from '@/services/EventService.js'
 import GStore from '@/store'
 import OrganizerService from '@/services/OrganizerService.js'
+import Login from '@/views/LoginForm.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'EventList',
-    component: EventList,
-    props: (route) => ({ page: parseInt(route.query.page) || 1 })
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/add-event',
-    name: 'AddEvent',
-    component: AddEvent,
-    beforeEnter: () => {
-      return OrganizerService.getOrganizers()
-        .then((response) => {
-          GStore.organizers = response.data
-        })
-        .catch(() => {
-          GStore.organizers = null
-          console.log('cannot load organizer')
-        })
-    }
-  },
-  {
-    path: '/event/:id',
-    name: 'EventLayout',
-    props: true,
-    component: EventLayout,
-    beforeEnter: (to) => {
-      return EventService.getEvent(to.params.id) // Return and params.id
-        .then((response) => {
-          // Still need to set the data here
-          GStore.event = response.data // <--- Store the event
-        })
-        .catch((error) => {
-          if (error.response && error.response.status == 404) {
-            return {
-              // <--- Return
-              name: '404Resource',
-              params: { resource: 'event' }
-            }
-          } else {
-            return { name: 'NetworkError' } // <--- Return
-          }
-        })
+const routes = [{
+        path: '/',
+        name: 'EventList',
+        component: EventList,
+        props: (route) => ({ page: parseInt(route.query.page) || 1 })
     },
-    children: [
-      {
-        path: '',
-        name: 'EventDetails',
-        component: EventDetails
-      },
-      {
-        path: 'register',
-        name: 'EventRegister',
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/add-event',
+        name: 'AddEvent',
+        component: AddEvent,
+        beforeEnter: () => {
+            return OrganizerService.getOrganizers()
+                .then((response) => {
+                    GStore.organizers = response.data
+                })
+                .catch(() => {
+                    GStore.organizers = null
+                    console.log('cannot load organizer')
+                })
+        }
+    },
+    {
+        path: '/event/:id',
+        name: 'EventLayout',
         props: true,
-        component: EventRegister
-      },
-      {
-        path: 'edit',
-        name: 'EventEdit',
-        props: true,
-        component: EventEdit
-      }
-    ]
-  },
-  {
-    path: '/404/:resource',
-    name: '404Resource',
-    component: NotFound,
-    props: true
-  },
-  {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
-    component: NotFound
-  },
-  {
-    path: '/network-error',
-    name: 'NetworkError',
-    component: NetWorkError
-  }
+        component: EventLayout,
+        beforeEnter: (to) => {
+            return EventService.getEvent(to.params.id) // Return and params.id
+                .then((response) => {
+                    // Still need to set the data here
+                    GStore.event = response.data // <--- Store the event
+                })
+                .catch((error) => {
+                    if (error.response && error.response.status == 404) {
+                        return {
+                            // <--- Return
+                            name: '404Resource',
+                            params: { resource: 'event' }
+                        }
+                    } else {
+                        return { name: 'NetworkError' } // <--- Return
+                    }
+                })
+        },
+        children: [{
+                path: '',
+                name: 'EventDetails',
+                component: EventDetails
+            },
+            {
+                path: 'register',
+                name: 'EventRegister',
+                props: true,
+                component: EventRegister
+            },
+            {
+                path: 'edit',
+                name: 'EventEdit',
+                props: true,
+                component: EventEdit
+            }
+        ]
+    },
+    {
+        path: '/404/:resource',
+        name: '404Resource',
+        component: NotFound,
+        props: true
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
+    },
+    {
+        path: '/network-error',
+        name: 'NetworkError',
+        component: NetWorkError
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { top: 0 }
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { top: 0 }
+        }
     }
-  }
 })
 router.beforeEach(() => {
-  NProgress.start()
+    NProgress.start()
 })
 
 router.afterEach(() => {
-  NProgress.done()
+    NProgress.done()
 })
 export default router
